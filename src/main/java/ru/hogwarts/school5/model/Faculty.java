f5fe2da17089ddb24d85f0c4f85080e65f9132ed
UPUHAK <89092936911@mail.ru>
2025-03-07 00:31:54 +0300

    hw SQL is done

--- src/main/java/ru/hogwarts/school5/model/Faculty.java
+++ src/main/java/ru/hogwarts/school5/model/Faculty.java
@@ -1,9 +1,12 @@
 package ru.hogwarts.school5.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +19,18 @@ public class Faculty {
     private String name;
 
     private String color;
+
+    @OneToMany(mappedBy = "faculty")
+    @JsonIgnore
+    private List<Student> students;
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
 
     public Faculty() {
     }
--- src/main/java/ru/hogwarts/school5/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school5/repository/FacultyRepository.java
@@ -3,5 +3,9 @@ package ru.hogwarts.school5.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school5.model.Faculty;
 
+import java.util.List;
+
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    List<Faculty> findByNameIgnoreCaseOrColorIgnoreCase(String name, String color);
 }
